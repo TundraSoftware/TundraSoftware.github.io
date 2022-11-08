77b8b664528aaba955e508895a1ed365041c0570
TundraSoftware <112476105+TundraSoftware@users.noreply.github.com>
2022-11-08 01:05:22 +0100

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,15 @@
+const toggle = document.getElementById('toggleDark');
+const body = document.querySelector('body');
+
+toggle.addEventListener('click', function(){
+    this.classList.toggle('bi-moon-fill');
+    if(this.classList.toggle('bi-brightness-high-fill')){
+        body.style.background = 'linear-gradient(to bottom, rgba(255, 255, 255, 0.3) 0%, rgba(255, 255, 255, 0.3) 100%, #fff 100%)';
+        body.style.color = 'var(--bs-nero1)';
+        body.style.transition = '0.5s';
+    }else{
+        body.style.background = 'linear-gradient(to bottom, rgba(0, 0, 0, 0.7) 0%, rgba(0, 0, 0, 0.7) 100%, #000 100%)';
+        body.style.color = 'var(--bs-bianco1)';
+        body.style.transition = '0.5s';
+    }
+});
